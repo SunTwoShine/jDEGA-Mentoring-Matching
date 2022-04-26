74a2466f6f43530bbd78e5ecb1160ab4fed117fa
SunTwoShine <mwehmeyer@hotmail.de>
2022-04-26 10:14:26 +0200

    used build-in function of PyPdf2 getFields. included stackoverflow-link for the function def.

--- matching_recommendations.py
+++ matching_recommendations.py
@@ -14,52 +14,11 @@ path_output_file = sys.argv[3]
 
 #-----------------------------------------------------------------------
 # Functions for loading of filled out forms
-
-def _getFields(obj, tree=None, retval=None, fileobj=None):
-    """
-    Extracts field data if this PDF contains interactive form fields.
-    The *tree* and *retval* parameters are for recursive use.
-
-    :param fileobj: A file object (usually a text file) to write
-        a report to on all interactive form fields found.
-    :return: A dictionary where each key is a field name, and each
-        value is a :class:`Field<PyPDF2.generic.Field>` object. By
-        default, the mapping name is used for keys.
-    :rtype: dict, or ``None`` if form data could not be located.
-    """
-    fieldAttributes = {'/FT': 'Field Type', '/Parent': 'Parent', '/T': 'Field Name', '/TU': 'Alternate Field Name',
-                       '/TM': 'Mapping Name', '/Ff': 'Field Flags', '/V': 'Value', '/DV': 'Default Value'}
-    if retval is None:
-        retval = OrderedDict()
-        catalog = obj.trailer["/Root"]
-        # get the AcroForm tree
-        if "/AcroForm" in catalog:
-            tree = catalog["/AcroForm"]
-        else:
-            return None
-    if tree is None:
-        return retval
-
-    obj._checkKids(tree, retval, fileobj)
-    for attr in fieldAttributes:
-        if attr in tree:
-            # Tree is a field
-            obj._buildField(tree, retval, fileobj, fieldAttributes)
-            break
-
-    if "/Fields" in tree:
-        fields = tree["/Fields"]
-        for f in fields:
-            field = f.getObject()
-            obj._buildField(field, retval, fileobj, fieldAttributes)
-
-    return retval
-
-
+# Answer from https://stackoverflow.com/a/43680515 by user dvska
 
 def get_form_fields(infile):
     infile = PdfFileReader(open(infile, 'rb'))
-    fields = _getFields(infile)
+    fields = PdfFileReader.getFields(infile)
     return OrderedDict((k, v.get('/V', '')) for k, v in fields.items())
 
 
